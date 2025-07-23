9aabb3e7dfd454c8bf6ffb10ea303f979e37a599
eagleman124 <dylanpoll543@gmail.com>
2025-07-23 17:48:22 -0600

    Add files via upload

--- content.js
+++ content.js
@@ -1,40 +1,24 @@
 
-function replaceTextAndAttributes(node) {
-    if (node.nodeType === Node.TEXT_NODE) {
-        if (node.textContent.includes("Connections")) {
-            node.textContent = node.textContent.replace(/Connections/g, "Friends");
-        }
+function replaceConnections(node) {
+  if (node.nodeType === 3) {
+    node.textContent = node.textContent
+      .replace(/\bConnections\b/g, "Friends")
+      .replace(/\bConnection\b/g, "Friend")
+      .replace(/\bConnect\b/g, "Friends");
+  } else if (node.nodeType === 1) {
+    for (let child of node.childNodes) {
+      replaceConnections(child);
     }
-
-    if (node.nodeType === Node.ELEMENT_NODE) {
-        ['aria-label', 'title', 'placeholder'].forEach(attr => {
-            if (node.hasAttribute(attr)) {
-                const val = node.getAttribute(attr);
-                if (val.includes("Connections")) {
-                    node.setAttribute(attr, val.replace(/Connections/g, "Friends"));
-                }
-            }
-        });
-
-        node.childNodes.forEach(child => replaceTextAndAttributes(child));
-    }
+  }
 }
 
-function replaceAll() {
-    replaceTextAndAttributes(document.body);
-}
-
-replaceAll();
-
-const observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-        mutation.addedNodes.forEach(node => {
-            replaceTextAndAttributes(node);
-        });
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      replaceConnections(node);
     });
+  });
 });
 
-observer.observe(document.body, {
-    childList: true,
-    subtree: true
-});
+replaceConnections(document.body);
+observer.observe(document.body, { childList: true, subtree: true });
